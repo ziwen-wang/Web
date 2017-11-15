e5fbdbffb249ad8577c97529c679319b45f739b8
ziwenwang <wangziwen.88888@foxmail.com>
2017-11-15 15:29:14 +0800

    多页面cli模板

---
+++ vue-cli-multipage/src/components/DiaLog/DiaLog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="masker-wp" v-show="dialog">
+    <div class="content">
+      {{msg}}
+      <button v-on:click="dialogHidden">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      dialog:true
+    }
+  },
+  methods: {
+    dialogHidden:function(){
+      this.dialog = false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .masker-wp{
+    position: fixed;
+    width:100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0,0,0,.5);
+  }
+  .masker-wp .content{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 300px;
+    height: 150px;
+    background: #fff;
+    margin: auto;
+  }
+</style>
---
+++ vue-cli-multipage/src/module/index/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    <hello></hello>
+    <v-dialog></v-dialog>
+    <button v-on:click='showMasker'>click me</button>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import Hello from 'components/Hello/Hello'
+  import DiaLog from 'components/DiaLog/DiaLog'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello,
+      'v-dialog':DiaLog
+    },
+    methods: {
+      showMasker:function(){
+        this.dialog = false
+      }
+  }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
